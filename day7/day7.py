2af3108dc8e896336c1bfabb0aa2206a1dd89c90
Nkesho <nikushakeshelava@gmail.com>
2022-10-20 18:54:02 +0400

    my changes

---
+++ day7/day7.py
@@ -0,0 +1,43 @@
+# menu = ["xinkali", "mwvadi", "lobiani", "xachapuri", "qababi", "wyali"]
+# # menu.insert(3, "nayini")
+# menu.append("cecxli")
+# print(menu)
+# i=0
+# ammount_of_aa = 0
+# menu = []
+# while i < 5:
+#     offer = input("What did you prefer to eat: ")
+#     for char in offer:
+#         if char == "a":
+#             ammount_of_aa +=1
+#         if ammount_of_aa >=2:
+#             menu.append(offer)
+#             ammount_of_aa = 0
+#     i+=1
+
+
+# print(menu)
+# new_menu = []
+# menu = ["xinkali", "mwvadi", "lobiani", "xachapuri", "qababi", "wyali"]
+# for food in menu:
+#     if food[1] != "a":
+#         new_menu.append(food)
+
+# print (new_menu)
+
+# scores = [20, 43, 56, 73, 10, 6, 87, 45, 97]
+# scores.sort()
+# print(scores)
+
+# students = ["nika", "ana", "mamuka", "beqa", "beqa", "dachi", "iva", "farma"]
+# students.sort(reverse = True)
+# print(students)
+
+#1) daasortiret scores = [20, 43, 56, 73, 10, 6, 87, 45, 97]
+#sort() gareshe gamovitanot yvelaze magali cifri
+
+#while ciklit satitaod shevamowmot mis marjvniv mdgomi, romelic ufro didi iqneba, 
+# droebit 
+# mianichet maqsimumis mnishvneloba da shemdeg shevadarot mis marjvniv mdgoms
+
+#2)students = ["nika", "ana", "mamuka", "beqa", "beqa", "dachi", "iva", "farma"]
---
+++ day8/day8.py
@@ -0,0 +1,25 @@
+# def shekreba(num1, num2):
+#     print(num1 + num2)
+
+# shekreba(10, 20)
+
+# def calculate_max(scores):
+#     max_num = scores[0]
+
+#     for score in scores:
+#         if score > max_num:
+#             max_num = score
+    
+
+# print(max_num)
+
+# scores = ([20, 43, 56, 73, 10, 6, 87, 45, 97])
+# scores = ([20, 43, 56, ])
+# scores = ([20, 43, 56, 5050505050])
+
+# def pair_sum(arr1, arr2):
+#     for i in range(min([len(arr1), len(arr2)])):
+#         print(arr1[i]+arr2[i])
+
+# pair_sum([20,43,5,10], 
+#         [2,4,51])
